dc6f3a98cbea7929c9f3dd515a579a8e2e26c802
elviisch26 <elvis.mnt123@gmail.com>
2023-04-14 22:06:37 -0500

    Se arreglo el array de inscripcion

--- Tarea1/arrays.js
+++ Tarea1/arrays.js
@@ -16,11 +16,11 @@ const aspirante =[
 ]
 
 const inscripcion =[
-    {id:1, id_curso:1, id_aspirante:1, fecha:'6/42023', hora:'14:54:05', valorCancelodo:'35,40'},
-    {id:2, id_curso:1, id_aspirante:2, fecha:'6/42023', hora:'18:15:55', valorCancelodo:'20,50'},
-    {id:3, id_curso:1, id_aspirante:3, fecha:'6/42023', hora:'11:25:10', valorCancelodo:'35,40'},
-    {id:4, id_curso:1, id_aspirante:4, fecha:'6/42023', hora:'13:47:55', valorCancelodo:'20,50'},    
-    {id:5, id_curso:1, id_aspirante:5, fecha:'6/42023', hora:'20:23:15', valorCancelodo:'35,40'},
+    {id:1, id_curso:1, id_aspirante:1, fecha:'6/4/2023', hora:'14:54:05', valorCancelodo:'35,40'},
+    {id:2, id_curso:1, id_aspirante:2, fecha:'6/4/2023', hora:'18:15:55', valorCancelodo:'20,50'},
+    {id:3, id_curso:1, id_aspirante:3, fecha:'6/4/2023', hora:'11:25:10', valorCancelodo:'35,40'},
+    {id:4, id_curso:1, id_aspirante:4, fecha:'6/4/2023', hora:'13:47:55', valorCancelodo:'20,50'},    
+    {id:5, id_curso:1, id_aspirante:5, fecha:'6/4/2023', hora:'20:23:15', valorCancelodo:'35,40'},
 ]
 
 //Se exportan los arrays para poder utilizarlos en las funciones 
